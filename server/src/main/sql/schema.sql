47731648115c8090c88efb08d345b1f14402fc37
Aldayr <aldayr.ruiz@opendeusto.es>
2021-03-23 11:13:13 +0100

    feat: Adds channel schema

--- server/src/main/sql/schema.sql
+++ server/src/main/sql/schema.sql
@@ -14,3 +14,12 @@ CREATE TABLE user (
     role_name VARCHAR(20)  NOT NULL,
     FOREIGN KEY (role_name) REFERENCES user_role (role_name)
 );
+
+CREATE TABLE channel (
+    PRIMARY KEY (id),
+    id            CHAR(36)     NOT NULL,
+    title         VARCHAR(30)  NOT NULL,
+    link          VARCHAR(255) NOT NULL UNIQUE,
+    description   VARCHAR(255) NOT NULL,
+    img_url       VARCHAR(255)
+);
