33abac67cec8766fc43f62543acd742cc3958178
Javier Orbe <javier.orbe@opendeusto.es>
2021-03-24 19:20:58 +0100

    feat: Add user email ux to schema

--- server/src/main/sql/schema.sql
+++ server/src/main/sql/schema.sql
@@ -10,7 +10,9 @@ CREATE TABLE user (
     PRIMARY KEY (id),
     id        CHAR(36)     NOT NULL,
     username  VARCHAR(30)  NOT NULL,
-    email     VARCHAR(255) NOT NULL UNIQUE,
+    email     VARCHAR(254) NOT NULL UNIQUE,
     role_name VARCHAR(20)  NOT NULL,
     FOREIGN KEY (role_name) REFERENCES user_role (role_name)
 );
+
+CREATE UNIQUE INDEX ux_user_email on user (email);
