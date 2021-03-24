592ee946c4b557fb8a44be5bac016af029a681e0
Aldayr <aldayr.ruiz@opendeusto.es>
2021-03-24 18:35:12 +0100

    style: Sql schema and db confusion

--- server/src/main/sql/schema.sql
+++ server/src/main/sql/schema.sql
@@ -17,17 +17,23 @@ CREATE TABLE user (
 
 CREATE TABLE category (
     PRIMARY KEY (id),
-    id           CHAR(36)     NOT NULL,
-    name         VARCHAR(30)  NOT NULL UNIQUE
+    id          CHAR(36)     NOT NULL,
+    name        VARCHAR(30)  NOT NULL UNIQUE
 );
 
 CREATE TABLE channel (
     PRIMARY KEY (id),
-    id            CHAR(36)     NOT NULL,
-    title         VARCHAR(30)  NOT NULL,
-    link          VARCHAR(255) NOT NULL UNIQUE,
-    description   VARCHAR(255) NOT NULL,
-    img_url       VARCHAR(255),
-    category      VARCHAR(36) NOT NULL,
-    FOREIGN KEY (category) REFERENCES category (id)
+    id          CHAR(36)     NOT NULL,
+    title       VARCHAR(30)  NOT NULL,
+    link        VARCHAR(255) NOT NULL UNIQUE,
+    description VARCHAR(255) NOT NULL,
+    img_url     VARCHAR(255)
+);
+
+CREATE TABLE channel_categories (
+    channel_id  VARCHAR(36) NOT NULL,
+    category_id VARCHAR(36) NOT NULL,
+    FOREIGN KEY (channel_id) REFERENCES channel (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    PRIMARY KEY (channel_id, category_id)
 );
