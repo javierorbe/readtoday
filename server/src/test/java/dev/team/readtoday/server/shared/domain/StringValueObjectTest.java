f9501a45e6605dfa5cbf38c81eb7c9b8123527d5
Javier Orbe <javier.orbe@opendeusto.es>
2021-03-25 19:11:19 +0100

    test: StringValueObjectTest

---
+++ server/src/test/java/dev/team/readtoday/server/shared/domain/StringValueObjectTest.java
@@ -0,0 +1,44 @@
+package dev.team.readtoday.server.shared.domain;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+final class StringValueObjectTest {
+
+  @ParameterizedTest
+  @MethodSource("provideMatchingStringAndPattern")
+  void shouldNotThrowExceptionIfValueMatchesThePattern(String value, Pattern pattern) {
+    assertDoesNotThrow(() -> new StringValueObject(value, pattern));
+  }
+
+  private static Stream<Arguments> provideMatchingStringAndPattern() {
+    return Stream.of(
+        Arguments.of("bubble", Pattern.compile("b[aeiou]bble")),
+        Arguments.of("babble", Pattern.compile("b[aeiou]bble")),
+        Arguments.of("ar4", Pattern.compile("a.[0-9]"))
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideNotMatchingStringAndPattern")
+  void shouldThrowExceptionIfValueDoesNotMatchThePattern(String value, Pattern pattern) {
+    assertThrows(IllegalArgumentException.class, () -> new StringValueObject(value, pattern));
+  }
+
+  private static Stream<Arguments> provideNotMatchingStringAndPattern() {
+    return Stream.of(
+        Arguments.of("bwbble", Pattern.compile("b[aeiou]bble")),
+        Arguments.of("bablbe", Pattern.compile("b[aeiou]bble")),
+        Arguments.of("awi", Pattern.compile("a.[0-9]"))
+    );
+  }
+}
