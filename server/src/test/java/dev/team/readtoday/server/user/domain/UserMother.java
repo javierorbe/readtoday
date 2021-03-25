cad0ef73c76183f013632992a5844627629e7768
Javier Orbe <javier.orbe@opendeusto.es>
2021-03-25 18:44:03 +0100

    test: Use UsernameMother in UserMother

--- server/src/test/java/dev/team/readtoday/server/user/domain/UserMother.java
+++ server/src/test/java/dev/team/readtoday/server/user/domain/UserMother.java
@@ -1,16 +1,12 @@
 package dev.team.readtoday.server.user.domain;
 
-import com.github.javafaker.Faker;
-
 public enum UserMother {
   ;
-
-  private static final Faker faker = Faker.instance();
 
   public static User random() {
     return new User(
         UserId.random(),
-        new Username(faker.bothify("????.????###")),
+        UsernameMother.random(),
         EmailAddressMother.random(),
         Role.USER
     );
