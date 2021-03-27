b392268df13a52458bf1e002e352775db04e860b
Iria Ramos <iria.ramos@opendeusto.es>
2021-03-27 18:27:37 +0100

    CheckStyle corrected

--- client/src/main/java/dev/team/readtoday/client/gui/HomePageView.java
+++ client/src/main/java/dev/team/readtoday/client/gui/HomePageView.java
@@ -9,40 +9,39 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class HomePageView extends Application{
-	
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			//BorderPane root = new BorderPane();
-			Button btnlog = new Button("Login");
-			Button btnreg = new Button("Register");
-			btnlog.setPrefSize(150,25);
-			btnreg.setPrefSize(150,25);
-			VBox vbox = new VBox();
-			btnreg.setOnAction(new EventHandler<ActionEvent>(){
-				@Override  
-	            public void handle(ActionEvent arg0) { 
-					
-					vbox.getScene().getWindow().hide();
-					RegisterView reg = new RegisterView();
-					reg.start(primaryStage);
-					
-	            } 
-			});
-			vbox.setSpacing(50);
-			vbox.setAlignment(Pos.CENTER);
-			vbox.getChildren().addAll(btnlog,btnreg);
-			
-			Scene scene = new Scene(vbox,400,400);
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Home Page");
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	public static void main(String[] args) {
-		launch(args);
-	}
+public class HomePageView extends Application {
+
+  @Override
+public void start(Stage primaryStage) {
+    try {
+      //BorderPane root = new BorderPane();
+      Button btnlog = new Button("Login");
+      Button btnreg = new Button("Register");
+      btnlog.setPrefSize(150, 25);
+      btnreg.setPrefSize(150, 25);
+      VBox vbox = new VBox();
+      btnreg.setOnAction(new EventHandler<ActionEvent>() {
+        @Override
+      public void handle(ActionEvent arg0) { 
+          vbox.getScene().getWindow().hide();
+          RegisterView reg = new RegisterView();
+          reg.start(primaryStage);
+          }
+      });
+      vbox.setSpacing(50);
+      vbox.setAlignment(Pos.CENTER);
+      vbox.getChildren().addAll(btnlog, btnreg);
+
+      Scene scene = new Scene(vbox, 400, 400);
+      primaryStage.setScene(scene);
+      primaryStage.setTitle("Home Page");
+      primaryStage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
--- client/src/main/java/dev/team/readtoday/client/gui/RegisterView.java
+++ client/src/main/java/dev/team/readtoday/client/gui/RegisterView.java
@@ -13,50 +13,45 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class RegisterView extends Application{
-	
-	public void start(Stage primaryStage) {
-		try {
-			VBox vbox = new VBox();
-			BorderPane base = new BorderPane();
-			
-			HBox hbox1 = new HBox();
-			HBox hbox2 = new HBox();
-			
-			Label labeluser = new Label("Username");
-			TextField username = new TextField();
-			Label labelmail = new Label("E-mail");
-			TextField email = new TextField();
-			Button sumbit = new Button("Sumbit");
-			
-			sumbit.setOnAction(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent arg0) {
-					Handler.addNewUser(username, email);
-				}
-			});		
-			
-			hbox1.setSpacing(50);
-			hbox1.setAlignment(Pos.CENTER);
-			hbox1.getChildren().addAll(labeluser, username);
-			
-			hbox2.setSpacing(70);
-			hbox2.setAlignment(Pos.CENTER);
-			hbox2.getChildren().addAll(labelmail, email);
-			
-			vbox.setSpacing(10);
-			vbox.setAlignment(Pos.CENTER);
-			vbox.getChildren().addAll(hbox1, hbox2, sumbit);
-			
-			Scene scene = new Scene(vbox,400,400);
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Register");
-			primaryStage.show();
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+public class RegisterView extends Application {
 
+  public void start(Stage primaryStage) {
+    try {
+      BorderPane base = new BorderPane();  
+      Button sumbit = new Button("Sumbit");
+      TextField username = new TextField();
+      TextField email = new TextField();
+      sumbit.setOnAction(new EventHandler<ActionEvent>() {
+        @Override
+  public void handle(ActionEvent arg0) {
+          Handler.addNewUser(username, email);
+        }
+      });
+      
+      HBox hbox1 = new HBox();
+      Label labeluser = new Label("Username");
+      hbox1.setSpacing(50);
+      hbox1.setAlignment(Pos.CENTER);
+      hbox1.getChildren().addAll(labeluser, username);
+      
+      HBox hbox2 = new HBox();
+      Label labelmail = new Label("E-mail");
+      hbox2.setSpacing(70);
+      hbox2.setAlignment(Pos.CENTER);
+      hbox2.getChildren().addAll(labelmail, email);
+      
+      VBox vbox = new VBox();
+      vbox.setSpacing(10);
+      vbox.setAlignment(Pos.CENTER);
+      vbox.getChildren().addAll(hbox1, hbox2, sumbit);
+
+      Scene scene = new Scene(vbox, 400, 400);
+      primaryStage.setScene(scene);
+      primaryStage.setTitle("Register");
+      primaryStage.show();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
