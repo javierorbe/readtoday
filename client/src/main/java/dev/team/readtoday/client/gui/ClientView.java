27e65bf5a24a9f2d9553a5ca68bdea68b8f61640
Iria Ramos <iria.ramos@opendeusto.es>
2021-03-31 19:40:27 +0200

    feat: Added GoogleAuth link(incomplete)

--- client/src/main/java/dev/team/readtoday/client/gui/ClientView.java
+++ client/src/main/java/dev/team/readtoday/client/gui/ClientView.java
@@ -41,21 +41,16 @@ public class ClientView extends Application {
     try {
       Stage stage = (Stage) register.getScene().getWindow();
       stage.close();
-      open("https://accounts.google.com/o/oauth2/v2/auth");
-
-      stage.setTitle("Register");
-      FXMLLoader loader = new FXMLLoader();
-      loader.setLocation(getClass().getResource("fxml/Register.fxml"));
-      ScrollPane rootLayout = (ScrollPane) loader.load();
-
-      Scene scene = new Scene(rootLayout);
-      stage.setScene(scene);
-      stage.show();
-
+      open("https://accounts.google.com/o/oauth2/v2/auth?"
+          + "scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly&"
+          + "access_type=offline&" + "include_granted_scopes=true&" + "response_type=code&"
+          + "state=state_parameter_passthrough_value&"
+          + "redirect_uri=https%3A//oauth2.example.com/code&" + "client_id=client_id");
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
+  //
 
   public void open(String url) throws Exception {
     URI u = new URI(url);
