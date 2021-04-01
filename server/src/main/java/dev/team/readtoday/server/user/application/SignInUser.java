d7b91c3abb30fad7e16251d127d80a1bad0cf76b
ikerove <52752754+ikerove@users.noreply.github.com>
2021-04-01 17:02:55 +0200

    verificar usuario

---
+++ server/src/main/java/dev/team/readtoday/server/user/application/SignInUser.java
@@ -0,0 +1,25 @@
+package dev.team.readtoday.server.user.application;
+
+import dev.team.readtoday.server.user.domain.EmailAddress;
+import dev.team.readtoday.server.user.domain.Role;
+import dev.team.readtoday.server.user.domain.User;
+import dev.team.readtoday.server.user.domain.UserRepository;
+import dev.team.readtoday.server.user.domain.Username;
+
+public class SignInUser {
+	private final ProfileFetcher profileFetcher;
+	private final UserRepository repository;
+	
+	public SignInUser(ProfileFetcher profileFetcher, UserRepository repository) {
+		this.profileFetcher = profileFetcher;
+		this.repository = repository;
+	}
+	
+	public Username SignIn(AuthToken token, Username username) throws AuthProcessFailed{
+		EmailAddress email = profileFetcher.fetchEmailAddress(token);
+		
+		
+	    return username;
+	}
+}
+
