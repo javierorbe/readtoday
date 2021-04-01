0a457f3f48da5de7fb179092e0d7221cd6f0ee65
Javier Orbe <javier.orbe@opendeusto.es>
2021-04-01 16:00:33 +0200

    feat: Update AuthView & add config file

--- client/src/main/java/dev/team/readtoday/client/App.java
+++ client/src/main/java/dev/team/readtoday/client/App.java
@@ -7,12 +7,15 @@ import com.google.gson.stream.JsonReader;
 import dev.team.readtoday.client.jersey.JerseyAuthController;
 import dev.team.readtoday.client.model.Category;
 import dev.team.readtoday.client.model.Channel;
+import dev.team.readtoday.client.oauth.AuthResponseServer;
+import dev.team.readtoday.client.oauth.JwtTokenReceiver;
 import dev.team.readtoday.client.view.auth.AuthController;
 import dev.team.readtoday.client.view.auth.AuthView;
 import dev.team.readtoday.client.view.home.HomeView;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.UriBuilder;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -21,13 +24,18 @@ import java.net.URL;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public final class App extends Application {
+public final class App extends Application implements JwtTokenReceiver {
 
   private static final Collection<Channel> EXAMPLE_CHANNELS = ImmutableSet.of(
       new Channel(UUID.randomUUID(),
@@ -47,27 +55,49 @@ public final class App extends Application {
       )
   );
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
   private static final double WINDOW_WIDTH = 600.0;
   private static final double WINDOW_HEIGHT = 400.0;
 
   private static final String CONFIG_FILE = "/config.json";
   private static final Gson GSON = new Gson();
 
+  private static final long AUTH_RESPONSE_LISTENER_SHUTDOWN_DELAY = 5L;
+
   private Scene homeScene;
   private Scene authScene;
+
+  private AuthResponseServer oAuthRespListener;
 
   @Override
   public void init() throws IOException {
     JsonObject config = loadConfig();
-    String baseUri = config.get("baseUri").getAsString();
 
+    URI oAuthBaseRedirectUri = URI.create(config.get("oAuthBaseRedirectUri").getAsString());
+    URI googleOauthBaseUri = URI.create(config.get("googleOauthBaseUri").getAsString());
+    String googleClientId = config.get("googleClientId").getAsString();
+
+    URI oAuthRedirectUri = UriBuilder.fromUri(oAuthBaseRedirectUri)
+        .path("oauth")
+        .build();
+
+    URI googleOauthUri = UriBuilder.fromUri(googleOauthBaseUri)
+        .queryParam("client_id", googleClientId)
+        .queryParam("redirect_uri", oAuthRedirectUri)
+        .build();
+
+    String baseUri = config.get("serverBaseUri").getAsString();
     Client client = ClientBuilder.newClient();
     WebTarget baseTarget = client.target(baseUri);
 
-    URI googleOauthUri = URI.create(config.get("googleOauthUri").getAsString());
     AuthController authController = new JerseyAuthController(baseTarget);
+    AuthView authView = new AuthView(googleOauthUri);
 
-    authScene = createAuthScene(authController, googleOauthUri);
+    oAuthRespListener =
+        new AuthResponseServer(oAuthBaseRedirectUri, authView, authController, this);
+
+    authScene = createScene("auth.fxml", authView);
     homeScene = createHomeScene();
   }
 
@@ -78,9 +108,13 @@ public final class App extends Application {
     stage.show();
   }
 
-  private static Scene createAuthScene(AuthController authController, URI googleOauthUri)
-      throws IOException {
-    return createScene("auth.fxml", new AuthView(authController, googleOauthUri));
+  @Override
+  public void receiveJwtToken(String jwtToken) {
+    LOGGER.debug("Received JWT token: {}", jwtToken);
+
+    ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+    executorService.schedule(() -> oAuthRespListener.close(),
+        AUTH_RESPONSE_LISTENER_SHUTDOWN_DELAY, TimeUnit.SECONDS);
   }
 
   private static Scene createHomeScene() throws IOException {
