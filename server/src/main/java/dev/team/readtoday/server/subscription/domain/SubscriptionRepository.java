000ba5fcf821f2da5e21e62a47db9e8e12ea2706
Daniel Urruchua <danielurrutxua@opendeusto.es>
2021-04-02 10:15:28 +0200

    feat: delete subscription

--- server/src/main/java/dev/team/readtoday/server/subscription/domain/SubscriptionRepository.java
+++ server/src/main/java/dev/team/readtoday/server/subscription/domain/SubscriptionRepository.java
@@ -1,6 +1,15 @@
 package dev.team.readtoday.server.subscription.domain;
 
+import java.util.List;
+import java.util.Optional;
+
+import dev.team.readtoday.server.user.domain.UserId;
+
+
 public interface SubscriptionRepository {
 
 	  void save(Subscription subscription);
+	  void remove(Subscription subscription);
+	  
+	  Optional<List<Subscription>> getAllByUserId(UserId userId);
 }
