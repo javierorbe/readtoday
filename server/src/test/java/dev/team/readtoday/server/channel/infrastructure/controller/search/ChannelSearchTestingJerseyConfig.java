00ee1a80eb07abcd7a72717840eb838a84d57f5e
Javier Orbe <javier.orbe@opendeusto.es>
2021-04-03 20:27:39 +0200

    test: Fix and simplify channel tests

--- server/src/test/java/dev/team/readtoday/server/channel/infrastructure/controller/search/ChannelSearchTestingJerseyConfig.java
+++ server/src/test/java/dev/team/readtoday/server/channel/infrastructure/controller/search/ChannelSearchTestingJerseyConfig.java
@@ -6,18 +6,16 @@ import dev.team.readtoday.server.category.domain.CategoryRepository;
 import dev.team.readtoday.server.channel.application.SearchChannelsByCategory;
 import dev.team.readtoday.server.channel.domain.ChannelRepository;
 import dev.team.readtoday.server.shared.infrastructure.controller.JwtTokenManager;
-import dev.team.readtoday.server.user.domain.UserRepository;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 
-final class TestingJerseyConfig extends ResourceConfig {
+final class ChannelSearchTestingJerseyConfig extends ResourceConfig {
 
   private static final String READTODAY_SERVER_PACKAGE = "dev.team.readtoday.server";
 
-  TestingJerseyConfig(JwtTokenManager jwtTokenManager,
-      UserRepository userRepository,
-      ChannelRepository channelRepository,
-      CategoryRepository categoryRepository) {
+  ChannelSearchTestingJerseyConfig(JwtTokenManager jwtTokenManager,
+                                   ChannelRepository channelRepository,
+                                   CategoryRepository categoryRepository) {
 
     packages(READTODAY_SERVER_PACKAGE);
     register(new AbstractBinder() {
@@ -26,14 +24,11 @@ final class TestingJerseyConfig extends ResourceConfig {
         bind(jwtTokenManager).to(JwtTokenManager.class);
 
         SearchCategoryByName searchCategoryByName = new SearchCategoryByName(categoryRepository);
+        SearchChannelsByCategory searchChannelsByCat =
+            new SearchChannelsByCategory(channelRepository, searchCategoryByName);
 
         bind(new SearchCategoriesById(categoryRepository)).to(SearchCategoriesById.class);
-        bind(new SearchChannelsByCategory(channelRepository, searchCategoryByName)).to(SearchChannelsByCategory.class);
-        bind(new SearchCategoryByName(categoryRepository)).to(SearchCategoryByName.class);
-
-        bind(userRepository).to(UserRepository.class);
-        bind(channelRepository).to(ChannelRepository.class);
-        bind(categoryRepository).to(CategoryRepository.class);
+        bind(searchChannelsByCat).to(SearchChannelsByCategory.class);
       }
     });
   }
--- server/src/test/java/dev/team/readtoday/server/channel/infrastructure/controller/search/RunCucumberTest.java
+++ server/src/test/java/dev/team/readtoday/server/channel/infrastructure/controller/search/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "pretty")
+@CucumberOptions(plugin = "pretty", glue = "dev.team.readtoday.server.channel.infrastructure.controller.search")
 public class RunCucumberTest {
 
 }
