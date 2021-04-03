a39b1dad0af82a614125596441caa068010c9b33
Javier Orbe <javier.orbe@opendeusto.es>
2021-04-03 13:52:20 +0200

    fix: RequiresAuth & CategoryDoesNotExist status response

--- server/src/main/java/dev/team/readtoday/server/channel/application/SearchChannelsByCategory.java
+++ server/src/main/java/dev/team/readtoday/server/channel/application/SearchChannelsByCategory.java
@@ -3,11 +3,11 @@ package dev.team.readtoday.server.channel.application;
 import dev.team.readtoday.server.category.application.SearchCategoryByName;
 import dev.team.readtoday.server.category.domain.Category;
 import dev.team.readtoday.server.category.domain.CategoryName;
+import dev.team.readtoday.server.channel.domain.CategoryDoesNotExist;
 import dev.team.readtoday.server.channel.domain.Channel;
 import dev.team.readtoday.server.channel.domain.ChannelRepository;
 import java.util.Collection;
 import java.util.Optional;
-import java.util.Set;
 
 public final class SearchChannelsByCategory {
 
@@ -20,11 +20,9 @@ public final class SearchChannelsByCategory {
     this.searchCategoryByName = searchCategoryByName;
   }
 
-  public Collection<Channel> search(CategoryName categoryName) {
-    Optional<Category> category = searchCategoryByName.search(categoryName);
-    if (category.isPresent()) {
-      return repository.getByCategory(category.get().getId());
-    }
-    return Set.of();
+  public Collection<Channel> search(CategoryName categoryName) throws CategoryDoesNotExist {
+    Optional<Category> optCategory = searchCategoryByName.search(categoryName);
+    Category category = optCategory.orElseThrow(() -> new CategoryDoesNotExist(categoryName));
+    return repository.getByCategory(category.getId());
   }
 }
