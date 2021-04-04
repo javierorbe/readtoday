b0f092b0a3028246914595e719252bf45e01b508
Iria Ramos <iria.ramos@opendeusto.es>
2021-04-04 10:03:27 +0200

    feat: Finished sign in controller

--- client/src/main/java/dev/team/readtoday/client/jersey/JerseyAuthController.java
+++ client/src/main/java/dev/team/readtoday/client/jersey/JerseyAuthController.java
@@ -26,7 +26,6 @@ public final class JerseyAuthController implements AuthController {
 
   @Override
   public String signIn(String googleOauthToken) {
-    // TODO: Implement method.
     SingInRequest request = new SingInRequest(googleOauthToken);
     Response response = singInTarget.request(MediaType.APPLICATION_JSON)
         .post(Entity.entity(request, MediaType.APPLICATION_JSON));
--- server/src/main/java/dev/team/readtoday/server/user/application/SignInUser.java
+++ server/src/main/java/dev/team/readtoday/server/user/application/SignInUser.java
@@ -1,25 +1,27 @@
 package dev.team.readtoday.server.user.application;
 
+import java.util.Optional;
 import dev.team.readtoday.server.user.domain.EmailAddress;
-import dev.team.readtoday.server.user.domain.Role;
+import dev.team.readtoday.server.user.domain.InvalidEmailAddress;
 import dev.team.readtoday.server.user.domain.User;
 import dev.team.readtoday.server.user.domain.UserRepository;
-import dev.team.readtoday.server.user.domain.Username;
 
 public class SignInUser {
-	private final ProfileFetcher profileFetcher;
-	private final UserRepository repository;
-	
-	public SignInUser(ProfileFetcher profileFetcher, UserRepository repository) {
-		this.profileFetcher = profileFetcher;
-		this.repository = repository;
-	}
-	
-	public Username SignIn(AuthToken token, Username username) throws AuthProcessFailed{
-		EmailAddress email = profileFetcher.fetchEmailAddress(token);
-		
-		
-	    return username;
-	}
+  private final ProfileFetcher profileFetcher;
+  private final UserRepository repository;
+
+  public SignInUser(ProfileFetcher profileFetcher, UserRepository repository) {
+    this.profileFetcher = profileFetcher;
+    this.repository = repository;
+  }
+
+  public User SignIn(AuthToken token) throws AuthProcessFailed, InvalidEmailAddress {
+    EmailAddress email = profileFetcher.fetchEmailAddress(token);
+    Optional<User> optUser = repository.getByEmailAddress(email);
+    if (!optUser.isPresent()) {
+      throw new InvalidEmailAddress("There aren't any users with that email: " + email);
+    }
+    return optUser.get();
+  }
 }
 
