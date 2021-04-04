f942b451b106c4cf2dec776980c33a5b6f17ab71
Iria Ramos <iria.ramos@opendeusto.es>
2021-04-04 10:03:27 +0200

    feat: Finished sign in controller

--- server/src/main/java/dev/team/readtoday/server/user/domain/InvalidEmailAddress.java
+++ server/src/main/java/dev/team/readtoday/server/user/domain/InvalidEmailAddress.java
@@ -7,7 +7,7 @@ public final class InvalidEmailAddress extends RuntimeException {
   @Serial
   private static final long serialVersionUID = -1427197240199832177L;
 
-  InvalidEmailAddress(String message) {
+  public InvalidEmailAddress(String message) {
     super(message);
   }
 }
