c16f336c98dd835e9bdaaeb76734f81f785ea015
Javier Orbe <javier.orbe@opendeusto.es>
2021-04-05 15:55:36 +0200

    fix: Create subscription service & controller
    - The create subscription service should receive the UserId and ChannelId.
    - The subscription controller should response with 201 status when succeeded.
    - The subscripiton creation request should only contain the ChannelId.

--- server/src/main/java/dev/team/readtoday/server/subscription/infrastructure/controller/SubscriptionRequest.java
+++ server/src/main/java/dev/team/readtoday/server/subscription/infrastructure/controller/SubscriptionRequest.java
@@ -1,37 +1,14 @@
 package dev.team.readtoday.server.subscription.infrastructure.controller;
 
-import dev.team.readtoday.server.channel.domain.ChannelId;
-import dev.team.readtoday.server.subscription.domain.Subscription;
-import dev.team.readtoday.server.user.domain.UserId;
-
 public class SubscriptionRequest {
 
-  private String idUser;
-  private String idChannel;
+  private String channelId;
 
-  public SubscriptionRequest(String idUser, String idChannel) {
-    this.idUser = idUser;
-    this.idChannel = idChannel;
+  public void setChannelId(String channelId) {
+    this.channelId = channelId;
   }
 
-  public String getUserId() {
-    return this.idUser;
-  }
-
-  public void setUserId(String idUser) {
-    this.idUser = idUser;
-  }
-
-  public String getChannelId() {
-    return this.idChannel;
-  }
-
-  public void setChannelId(String idChannel) {
-    this.idChannel = idChannel;
-  }
-
-  public Subscription toSubscription() {
-    return new Subscription(UserId.fromString(idUser), ChannelId.fromString(idChannel));
-
+  String getChannelId() {
+    return channelId;
   }
 }
