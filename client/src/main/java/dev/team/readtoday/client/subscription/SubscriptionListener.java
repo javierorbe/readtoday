2a76868288a8436b2089ee7333c9f7bdef44ede8
Iria Ramos <iria.ramos@opendeusto.es>
2021-04-05 12:24:25 +0200

    feat: Subscription in client

---
+++ client/src/main/java/dev/team/readtoday/client/subscription/SubscriptionListener.java
@@ -0,0 +1,43 @@
+package dev.team.readtoday.client.subscription;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import dev.team.readtoday.client.create.ChannelCreationEvent;
+import dev.team.readtoday.client.create.ChannelCreationResponseEvent;
+import dev.team.readtoday.client.storage.UserJwtTokenStorage;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+public class SubscriptionListener {
+
+  Logger LOGGER = LoggerFactory.getLogger(SubscriptionListener.class);
+
+  private final EventBus eventBus;
+  private final WebTarget subscriptionTarget;
+
+  private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+  public SubscriptionListener(EventBus eventBus, WebTarget subscriptionTarget) {
+    this.eventBus = eventBus;
+    this.subscriptionTarget = subscriptionTarget.path("/subscription");
+  }
+
+  @Subscribe
+  public void onChannelCreationRequestReceived(ChannelCreationEvent event) {
+
+    LOGGER.trace("Sending subscription request.");
+    executorService.submit(() -> {
+      Response response = subscriptionTarget.request(MediaType.APPLICATION_JSON)
+          .header(HttpHeaders.AUTHORIZATION, "Bearer " + UserJwtTokenStorage.getToken())
+          .post(Entity.entity(event.getRequest(), MediaType.APPLICATION_JSON));
+      eventBus.post(new ChannelCreationResponseEvent(response));
+    });
+  }
+}
