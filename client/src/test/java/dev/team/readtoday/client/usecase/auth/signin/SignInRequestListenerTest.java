5c74ee2e779db1c8136e558bc8b486a9c7987af3
Javier Orbe <javier.orbe@opendeusto.es>
2021-04-13 18:27:09 +0200

    test(client): Fix SignInRequestListenerTest

--- client/src/test/java/dev/team/readtoday/client/usecase/auth/signin/SignInRequestListenerTest.java
+++ client/src/test/java/dev/team/readtoday/client/usecase/auth/signin/SignInRequestListenerTest.java
@@ -3,12 +3,12 @@ package dev.team.readtoday.client.usecase.auth.signin;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.google.common.eventbus.EventBus;
 import dev.team.readtoday.client.usecase.shared.HttpRequestBuilder;
+import dev.team.readtoday.client.usecase.shared.HttpRequestBuilderFactory;
 import dev.team.readtoday.client.usecase.shared.HttpResponse;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -20,19 +20,24 @@ final class SignInRequestListenerTest {
 
   @Test
   void shouldPostSuccessfulSignInEventIfSucceeded() {
-    EventBus eventBus = mock(EventBus.class);
+    // Given
+    HttpRequestBuilderFactory factory = mock(HttpRequestBuilderFactory.class);
     HttpRequestBuilder requestBuilder = mock(HttpRequestBuilder.class);
-    SignInRequestListener listener = new SignInRequestListener(eventBus, requestBuilder);
+    when(factory.build("/auth/signin")).thenReturn(requestBuilder);
     HttpResponse response = mock(HttpResponse.class);
     when(requestBuilder.post(any())).thenReturn(response);
     String jwtToken = "someJwtToken";
     when(response.getEntity(String.class)).thenReturn(jwtToken);
     when(response.isStatusOk()).thenReturn(true);
+    EventBus eventBus = mock(EventBus.class);
+    SignInRequestListener listener = new SignInRequestListener(eventBus, factory);
 
+    // When
     listener.signIn(new SignInRequestReadyEvent("someAccessToken"));
 
-    ArgumentCaptor<SuccessfulSignInEvent> eventCaptor = ArgumentCaptor.forClass(SuccessfulSignInEvent.class);
-    verify(eventBus, times(1)).post(eventCaptor.capture());
+    // Then
+    var eventCaptor = ArgumentCaptor.forClass(SuccessfulSignInEvent.class);
+    verify(eventBus).post(eventCaptor.capture());
     assertEquals(jwtToken, eventCaptor.getValue().getJwtToken());
   }
 }
