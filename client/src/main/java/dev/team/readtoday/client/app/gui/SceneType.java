8307683b7728210a32f5a1fe1c666e0874239730
Javier Orbe <javier.orbe@opendeusto.es>
2021-04-13 18:27:17 +0200

    feat(client): SceneContainer

--- client/src/main/java/dev/team/readtoday/client/app/gui/SceneType.java
+++ client/src/main/java/dev/team/readtoday/client/app/gui/SceneType.java
@@ -1,5 +1,17 @@
 package dev.team.readtoday.client.app.gui;
 
 public enum SceneType {
-  AUTH, HOME, ADMIN
+  AUTH("auth.fxml"),
+  HOME("home.fxml"),
+  ADMIN("channelCreation.fxml");
+
+  private final String fxmlFile;
+
+  SceneType(String fxmlFile) {
+    this.fxmlFile = fxmlFile;
+  }
+
+  public String getFxmlFile() {
+    return fxmlFile;
+  }
 }
