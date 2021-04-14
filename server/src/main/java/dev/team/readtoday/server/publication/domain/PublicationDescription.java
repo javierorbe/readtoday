1b622b7c42daf93a885899eba120a114cfe5af02
Javier Orbe <javier.orbe@opendeusto.es>
2021-04-14 17:39:03 +0200

    feat: Publication model

---
+++ server/src/main/java/dev/team/readtoday/server/publication/domain/PublicationDescription.java
@@ -0,0 +1,10 @@
+package dev.team.readtoday.server.publication.domain;
+
+import dev.team.readtoday.server.shared.domain.StringValueObject;
+
+public final class PublicationDescription extends StringValueObject {
+
+  public PublicationDescription(String value) {
+    super(value);
+  }
+}
