a0aa202958a5a3278e322b0ac8946bc8fcfc630b
ikerove <52752754+ikerove@users.noreply.github.com>
2021-04-14 17:35:26 +0200

    feat: Subscribe List

---
+++ server/src/main/java/dev/team/readtoday/server/subscription/infrastructure/controller/get/SubscriptionGetController.java
@@ -0,0 +1,21 @@
+package dev.team.readtoday.server.subscription.infrastructure.controller.get;
+
+import dev.team.readtoday.server.shared.infrastructure.controller.BaseController;
+import dev.team.readtoday.server.subscription.application.DeleteSubscription;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+
+//@Path("subscriptions")
+public final class SubscriptionGetController extends BaseController {
+    // atributo1
+   // @Autowired
+    public SubscriptionGetController(/*atributo1*/) {
+
+    }
+
+
+    /*public Response getSubscriptions(){
+        return response();
+    }*/
+}
