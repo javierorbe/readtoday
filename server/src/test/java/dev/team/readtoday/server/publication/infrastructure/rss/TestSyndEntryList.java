69464d76ab4e09cbdf0ad430c08173826d34b415
Javier Orbe <javier.orbe@opendeusto.es>
2021-04-17 18:16:20 +0200

    test: RomeRssFetcherTest remove mock

--- server/src/test/java/dev/team/readtoday/server/publication/infrastructure/rss/TestSyndEntryList.java
+++ server/src/test/java/dev/team/readtoday/server/publication/infrastructure/rss/TestSyndEntryList.java
@@ -9,40 +9,14 @@ import dev.team.readtoday.server.publication.domain.PublicationLink;
 import dev.team.readtoday.server.publication.domain.PublicationTitle;
 import dev.team.readtoday.server.shared.domain.CategoryId;
 import dev.team.readtoday.server.shared.domain.PublicationId;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Arrays;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.Collection;
-import java.util.Date;
 import java.util.List;
 import java.util.stream.Stream;
 
 enum TestSyndEntryList {
   ;
-
-  static List<SyndEntryFake> buildEntryList() {
-    ImmutableList.Builder<SyndEntryFake> builder = new Builder<>();
-
-    builder.add(new SyndEntryFake(
-        "https://techcrunch.com/?p=2138596",
-        "Greylock GP Mike Duboe to discuss how to scale your company at TechCrunch Early Stage in July",
-        "While the money flowing into Silicon Valley is reaching historic heights",
-        toDate(LocalDateTime.of(2021, 4, 15, 15, 49, 54)),
-        "http://feedproxy.google.com/~r/Techcrunch/~3/cjCF-LHVGiQ/",
-        toSyndCategoryList("Events", "Startups")
-    ));
-
-    builder.add(new SyndEntryFake(
-        "https://techcrunch.com/?p=2139185",
-        "Casa Blanca raises $2.6M to build the ‘Bumble for real estate’",
-        "Casa Blanca, which aims to develop a “Bumble-like app” for finding a home",
-        toDate(LocalDateTime.of(2021, 4, 15, 15, 32, 23)),
-        "http://feedproxy.google.com/~r/Techcrunch/~3/TgqvYS0lvtk/",
-        toSyndCategoryList("Fundings & Exists", "Real Estate", "Startups")
-    ));
-
-    return builder.build();
-  }
 
   static List<Publication> buildPublicationList() {
     ImmutableList.Builder<Publication> builder = new Builder<>();
@@ -51,32 +25,24 @@ enum TestSyndEntryList {
         new PublicationId("https://techcrunch.com/?p=2138596"),
         new PublicationTitle("Greylock GP Mike Duboe to discuss how to scale your company at TechCrunch Early Stage in July"),
         new PublicationDescription("While the money flowing into Silicon Valley is reaching historic heights"),
-        new PublicationDate(LocalDateTime.of(2021, 4, 15, 15, 49, 54)),
+        new PublicationDate(OffsetDateTime.of(2021, 4, 15, 15, 49, 54, 0, ZoneOffset.UTC)),
         new PublicationLink("http://feedproxy.google.com/~r/Techcrunch/~3/cjCF-LHVGiQ/"),
-        toDomainCategoryCollection(2)
+        toDomainCategoryCollection(2L)
     ));
 
     builder.add(new Publication(
         new PublicationId("https://techcrunch.com/?p=2139185"),
         new PublicationTitle("Casa Blanca raises $2.6M to build the ‘Bumble for real estate’"),
         new PublicationDescription("Casa Blanca, which aims to develop a “Bumble-like app” for finding a home"),
-        new PublicationDate(LocalDateTime.of(2021, 4, 15, 15, 32, 23)),
+        new PublicationDate(OffsetDateTime.of(2021, 4, 15, 15, 32, 23, 0, ZoneOffset.UTC)),
         new PublicationLink("http://feedproxy.google.com/~r/Techcrunch/~3/TgqvYS0lvtk/"),
-        toDomainCategoryCollection(3)
+        toDomainCategoryCollection(3L)
     ));
 
     return builder.build();
   }
 
-  private static Date toDate(LocalDateTime dateTime) {
-    return Date.from(dateTime.atZone(ZoneId.systemDefault()).toInstant());
-  }
-
-  private static List<CategoryFake> toSyndCategoryList(String... categoryNames) {
-    return Arrays.stream(categoryNames).map(CategoryFake::new).toList();
-  }
-
-  private static Collection<CategoryId> toDomainCategoryCollection(int count) {
+  private static Collection<CategoryId> toDomainCategoryCollection(long count) {
     return Stream.generate(CategoryId::random).limit(count).toList();
   }
 }
