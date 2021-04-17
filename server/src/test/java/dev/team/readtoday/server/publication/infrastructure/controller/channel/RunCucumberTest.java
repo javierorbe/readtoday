33792ced7af529b5c61d742415eac46abb3a1f04
Javier Orbe <javier.orbe@opendeusto.es>
2021-04-17 18:17:08 +0200

    test: GetChannelPublicationsFeature

---
+++ server/src/test/java/dev/team/readtoday/server/publication/infrastructure/controller/channel/RunCucumberTest.java
@@ -0,0 +1,11 @@
+package dev.team.readtoday.server.publication.infrastructure.controller.channel;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "pretty", glue = "dev.team.readtoday.server.publication.infrastructure.controller.channel")
+public class RunCucumberTest {
+
+}
