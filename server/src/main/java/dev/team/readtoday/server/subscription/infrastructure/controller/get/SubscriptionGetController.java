c2514fe9c29a4cfe6878b6162f654b6a1bd8ef2b
ikerove <52752754+ikerove@users.noreply.github.com>
2021-04-20 13:55:36 +0200

    feat: Controller subscription list

--- server/src/main/java/dev/team/readtoday/server/subscription/infrastructure/controller/get/SubscriptionGetController.java
+++ server/src/main/java/dev/team/readtoday/server/subscription/infrastructure/controller/get/SubscriptionGetController.java
@@ -1,11 +1,20 @@
 package dev.team.readtoday.server.subscription.infrastructure.controller.get;
 
 
+import dev.team.readtoday.server.category.application.SearchAllCategories;
+import dev.team.readtoday.server.category.application.search.SearchCategory;
+import dev.team.readtoday.server.category.domain.Category;
+import dev.team.readtoday.server.category.domain.CategoryName;
+import dev.team.readtoday.server.channel.application.SearchChannelsFromSubscriptions;
+import dev.team.readtoday.server.channel.domain.Channel;
+import dev.team.readtoday.server.shared.domain.CategoryId;
 import dev.team.readtoday.server.shared.infrastructure.controller.BaseController;
+import dev.team.readtoday.server.shared.infrastructure.controller.authfilter.RequiresAuth;
 import dev.team.readtoday.server.subscription.application.GetUserSubscriptions;
 import dev.team.readtoday.server.subscription.domain.Subscription;
 import dev.team.readtoday.server.subscription.domain.SubscriptionNotFound;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.MediaType;
@@ -15,34 +24,47 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
-
+@RequiresAuth
 @Path("subscriptions")
 public final class SubscriptionGetController extends BaseController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SubscriptionGetController.class);
 
-  private final GetUserSubscriptions getUserSubscriptions;
+  private final SearchChannelsFromSubscriptions searchChannels;
+  private final SearchCategory searchCategoriesById;
+
+
   @Autowired
-  public SubscriptionGetController(GetUserSubscriptions getUserSubscriptions) {
-       this.getUserSubscriptions = getUserSubscriptions;
+  public SubscriptionGetController(SearchChannelsFromSubscriptions searchChannels, SearchCategory searchCategoriesById) {
+       this.searchChannels = searchChannels;
+       this.searchCategoriesById = searchCategoriesById;
 
   }
 
-  @POST
+  @GET
   @Consumes(MediaType.APPLICATION_JSON)
   public Response getSubscription() {
 
     try{
-        Collection<Subscription> subscriptions = getUserSubscriptions.search(getRequestUserId());
-        LOGGER.trace("Successful getting subscriptions request.");
-        return Response.ok(subscriptions).build();
-    }catch(SubscriptionNotFound e){
+        Collection<Channel> subscriptions = searchChannels.search(getRequestUserId());
+        Collection<CategoryId> categoryIds = flatChannelCategories(subscriptions);
+        Collection<Category> categories = searchCategoriesById.apply(categoryIds);
+        AllSubscriptionsResponse response = new AllSubscriptionsResponse(subscriptions, categories);
+        subscriptions.forEach(System.out::println);
+        return Response.ok(response).build();
+    } catch(SubscriptionNotFound e){
           LOGGER.debug("Error getting subscriptions.", e);
           return response(Response.Status.BAD_REQUEST);
     }
   }
 
+    private static Collection<CategoryId> flatChannelCategories(Collection<Channel> channels) {
+        return channels.stream()
+                .flatMap(channel -> channel.getCategories().stream())
+                .collect(Collectors.toSet());
+    }
 
 
 }
