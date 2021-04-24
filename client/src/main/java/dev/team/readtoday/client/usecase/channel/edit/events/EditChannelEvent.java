7c034776b3dfd2dd929e0dc19009246a577aea0d
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2021-04-24 16:01:35 +0200

    feat: Edit channel request client side

---
+++ client/src/main/java/dev/team/readtoday/client/usecase/channel/edit/events/EditChannelEvent.java
@@ -0,0 +1,23 @@
+package dev.team.readtoday.client.usecase.channel.edit.events;
+
+import dev.team.readtoday.client.usecase.channel.edit.messages.EditChannelRequest;
+
+public final class EditChannelEvent {
+
+  private final String channelId;
+  private final EditChannelRequest request;
+
+  public EditChannelEvent(String channelId,
+      EditChannelRequest request) {
+    this.channelId = channelId;
+    this.request = request;
+  }
+
+  public String getChannelId() {
+    return channelId;
+  }
+
+  public EditChannelRequest getRequest() {
+    return request;
+  }
+}
