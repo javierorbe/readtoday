a4d1e17a3b1056e8dc3cae5e00dddeb403cf130d
ikerove <52752754+ikerove@users.noreply.github.com>
2021-04-26 13:34:39 +0200

    test: Subscription list test

--- client/src/test/java/dev/team/readtoday/client/usecase/MySubscriptionListListenerTest.java
+++ client/src/test/java/dev/team/readtoday/client/usecase/MySubscriptionListListenerTest.java
@@ -1,17 +1,19 @@
 package dev.team.readtoday.client.usecase;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import dev.team.readtoday.client.usecase.auth.signin.SignInRequest;
-import dev.team.readtoday.client.usecase.auth.signin.SignInRequestListener;
-import dev.team.readtoday.client.usecase.auth.signin.SignInRequestReadyEvent;
+
 import dev.team.readtoday.client.usecase.shared.HttpRequestBuilder;
 import dev.team.readtoday.client.usecase.shared.HttpRequestBuilderFactory;
 import dev.team.readtoday.client.usecase.shared.HttpResponse;
 import dev.team.readtoday.client.usecase.subscription.search.MySubscriptionsListListener;
-import dev.team.readtoday.client.usecase.subscription.subscribe.SubscriptionRequestedEvent;
+import dev.team.readtoday.client.usecase.subscription.search.events.MySubscriptionsListFailedEvent;
+import dev.team.readtoday.client.usecase.subscription.search.events.MySubscriptionsListRequestedEvent;
+import dev.team.readtoday.client.usecase.subscription.search.events.SuccessfulMySubscriptionsListEvent;
 import org.greenrobot.eventbus.EventBus;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -22,27 +24,56 @@ import org.mockito.ArgumentCaptor;
 public class MySubscriptionListListenerTest {
 
   @Test
-  void shouldGetMySubscriptionsList(){
+  void shouldGetMySubscriptionsListSuccess(){
     // Given
-   /* HttpRequestBuilderFactory factory = mock(HttpRequestBuilderFactory.class);
+    HttpRequestBuilderFactory factory = mock(HttpRequestBuilderFactory.class);
     HttpRequestBuilder requestBuilder = mock(HttpRequestBuilder.class);
-    when(factory.build("/auth/signin")).thenReturn(requestBuilder);
+    when(factory.build("/subscriptions")).thenReturn(requestBuilder);
 
     HttpResponse response = mock(HttpResponse.class);
-    var signInRequestCaptor = ArgumentCaptor.forClass(SignInRequest.class);
-    when(requestBuilder.post(signInRequestCaptor.capture())).thenReturn(response);
-    when(response.isStatusOk()).thenReturn(false);
+    when(requestBuilder.get()).thenReturn(response);
+    when(response.isStatusOk()).thenReturn(true);
 
     EventBus eventBus = mock(EventBus.class);
-    var listener = new MySubscriptionsListListener(eventBus, factory);
+    MySubscriptionsListListener listener = new MySubscriptionsListListener(eventBus, factory);
 
     // When
 
-    //listener.onSubscriptionListRequested(new SubscriptionRequestedEvent(channel));
+    listener.onSubscriptionListRequested(new MySubscriptionsListRequestedEvent());
+// Then
+    var eventCaptor = ArgumentCaptor.forClass(SuccessfulMySubscriptionsListEvent.class);
+    verify(eventBus).post(eventCaptor.capture());
+    var event = eventCaptor.getValue();
+    assertEquals(event.getClass(),SuccessfulMySubscriptionsListEvent.class);
+  }
+
+  @Test
+  void shouldGetMySubscriptionsListFail(){
+    //Given
+    // Given
+    HttpRequestBuilderFactory factory = mock(HttpRequestBuilderFactory.class);
+    HttpRequestBuilder requestBuilder = mock(HttpRequestBuilder.class);
+    when(factory.build("/subscriptions")).thenReturn(requestBuilder);
+
+    HttpResponse response = mock(HttpResponse.class);
+    when(requestBuilder.get()).thenReturn(response);
+    when(response.isStatusOk()).thenReturn(false);
+    String reason = "Failed reason";
+    when(response.getStatusReason()).thenReturn(reason);
+
+    EventBus eventBus = mock(EventBus.class);
+    MySubscriptionsListListener listener = new MySubscriptionsListListener(eventBus, factory);
+
+    // When
+
+    listener.onSubscriptionListRequested(new MySubscriptionsListRequestedEvent());
 
     // Then
-    var signInRequest = signInRequestCaptor.getValue();
-    //assertEquals(accessToken, signInRequest.getAccessToken());*/
+    var eventCaptor = ArgumentCaptor.forClass(SuccessfulMySubscriptionsListEvent.class);
+    verify(eventBus).post(eventCaptor.capture());
+    var event = eventCaptor.getValue();
+    assertEquals(event.getClass(), MySubscriptionsListFailedEvent.class);
   }
 
 }
+
