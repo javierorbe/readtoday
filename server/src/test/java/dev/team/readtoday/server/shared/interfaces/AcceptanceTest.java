ca7de45472fa91548f2c945347d11836a1adc7f3
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2021-04-26 20:29:59 +0200

    refactor: Divide unit test and integration test
    Acceptance test are not executed in verify phase because spring scanning is failing now.

---
+++ server/src/test/java/dev/team/readtoday/server/shared/interfaces/AcceptanceTest.java
@@ -0,0 +1,5 @@
+package dev.team.readtoday.server.shared.interfaces;
+
+public interface AcceptanceTest {
+
+}
--- server/src/test/java/dev/team/readtoday/server/subscription/infrastructure/controller/post/RunCucumberTest.java
+++ server/src/test/java/dev/team/readtoday/server/subscription/infrastructure/controller/post/RunCucumberTest.java
@@ -1,11 +1,14 @@
 package dev.team.readtoday.server.subscription.infrastructure.controller.post;
 
+import dev.team.readtoday.server.shared.interfaces.AcceptanceTest;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = "pretty", glue = "dev.team.readtoday.server.subscription.infrastructure.controller.post")
+@Category(AcceptanceTest.class)
 public class RunCucumberTest {
 
 }
