e7bc212f06e098b81b5eac2669b860380e9e98e3
Daniel Urruchua <danielurrutxua@opendeusto.es>
2021-04-27 19:03:52 +0200

    feat(client): Edit channel GUI

--- client/src/main/java/dev/team/readtoday/client/app/gui/SceneType.java
+++ client/src/main/java/dev/team/readtoday/client/app/gui/SceneType.java
@@ -3,8 +3,8 @@ package dev.team.readtoday.client.app.gui;
 public enum SceneType {
   AUTH("auth.fxml"),
   HOME("home.fxml"),
-  ADMIN("channelCreation.fxml");
-
+  ADMIN("channelCreation.fxml"),
+  ADMIN1("channelEdition.fxml");
   private final String fxmlFile;
 
   SceneType(String fxmlFile) {
