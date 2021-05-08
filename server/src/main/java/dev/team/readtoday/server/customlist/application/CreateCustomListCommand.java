a13031a6d27059c3a49d1d157ee29916ac73b7e8
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2021-05-08 18:33:58 +0200

    feat: CustomList application layer

--- server/src/main/java/dev/team/readtoday/server/customlist/application/CreateCustomListCommand.java
+++ server/src/main/java/dev/team/readtoday/server/customlist/application/CreateCustomListCommand.java
@@ -1,5 +1,22 @@
 package dev.team.readtoday.server.customlist.application;
 
-public class CreateCustomListCommand {
-  // TODO: Implement this
+import dev.team.readtoday.server.shared.domain.bus.command.Command;
+
+public class CreateCustomListCommand implements Command {
+
+  private final String title;
+  private final String userId;
+
+  public CreateCustomListCommand(String title, String userId) {
+    this.title = title;
+    this.userId = userId;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
 }
