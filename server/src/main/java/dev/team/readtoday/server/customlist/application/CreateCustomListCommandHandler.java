222da34ee84286e5dbeba16b4865b3a188c304e5
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2021-05-08 11:11:16 +0200

    feat: Custom List create controller

---
+++ server/src/main/java/dev/team/readtoday/server/customlist/application/CreateCustomListCommandHandler.java
@@ -0,0 +1,5 @@
+package dev.team.readtoday.server.customlist.application;
+
+public class CreateCustomListCommandHandler {
+  // TODO: Implement this
+}
---
+++ server/src/main/java/dev/team/readtoday/server/customlist/infrastructure/controller/create/CustomListCreationRequest.java
@@ -0,0 +1,14 @@
+package dev.team.readtoday.server.customlist.infrastructure.controller.create;
+
+public final class CustomListCreationRequest {
+
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
