15dce7a41987fcef6b09919f75120be84c15ac61
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2021-05-08 18:52:10 +0200

    refactor: CustomList controller

--- server/src/main/java/dev/team/readtoday/server/customlist/infrastructure/controller/create/CustomListCreationRequest.java
+++ server/src/main/java/dev/team/readtoday/server/customlist/infrastructure/controller/create/CustomListCreationRequest.java
@@ -2,13 +2,13 @@ package dev.team.readtoday.server.customlist.infrastructure.controller.create;
 
 public final class CustomListCreationRequest {
 
-  private String name;
+  private String title;
 
-  public String getName() {
-    return name;
+  public String getTitle() {
+    return title;
   }
 
-  public void setName(String name) {
-    this.name = name;
+  public void setTitle(String title) {
+    this.title = title;
   }
 }
