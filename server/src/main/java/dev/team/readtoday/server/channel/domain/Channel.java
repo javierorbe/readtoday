fefc13ea2a1e0401aa20e78abcb6146bd92eddf3
Aldayr <aldayr.ruiz@opendeusto.es>
2021-05-11 12:41:43 +0200

    docs: Channel and ChannelRepository classes

--- server/src/main/java/dev/team/readtoday/server/channel/domain/Channel.java
+++ server/src/main/java/dev/team/readtoday/server/channel/domain/Channel.java
@@ -8,13 +8,34 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 
+/**
+ * Represents a Rss Channel.
+ */
 public final class Channel {
 
+  /**
+   * Represents the id (UUID) of the channel
+   */
   private final ChannelId id;
+  /**
+   * Represents the title or the name of the channel.
+   */
   private final ChannelTitle title;
+  /**
+   * Represents the link or url to the HTML website corresponding to the channel.
+   */
   private final RssUrl rssUrl;
+  /**
+   * Represents the phrase or sentence describing the channel.
+   */
   private final ChannelDescription description;
+  /**
+   * Represents the url of a GIF, JPEG or PNG image that represents the channel.
+   */
   private final ImageUrl imageUrl;
+  /**
+   * Represents channel's categories.
+   */
   private final Collection<CategoryId> categories;
 
   public Channel(
@@ -32,6 +53,16 @@ public final class Channel {
     this.categories = new HashSet<>(categories);
   }
 
+  /**
+   * Returns a channel instance, it manages to create a random id to the channel.
+   *
+   * @param title       the title or name of the new channel.
+   * @param rssUrl      the link or url to the HTML website corresponding to the channel.
+   * @param description phrase or sentence describing the channel.
+   * @param imageUrl    the image url of the channel.
+   * @param categoryIds list of category ids.
+   * @return a new channel with properties passed.
+   */
   public static Channel create(
       ChannelTitle title,
       RssUrl rssUrl,
