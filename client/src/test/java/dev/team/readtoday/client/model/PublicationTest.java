e345df90b5ae13d342367136621e242fb9df92a9
ikerove <52752754+ikerove@users.noreply.github.com>
2021-05-18 09:55:43 +0200

    fix: test

--- client/src/test/java/dev/team/readtoday/client/model/PublicationTest.java
+++ client/src/test/java/dev/team/readtoday/client/model/PublicationTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@Disabled
+
 @TestMethodOrder(MethodOrderer.Random.class)
 final class PublicationTest {
 
